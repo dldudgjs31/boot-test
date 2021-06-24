d42b4e25724cf069e897f8ba9b117534553ac7bc
User <User@192.168.10.58>
2021-06-24 17:41:26 +0900

    test

--- jboss/src/main/java/penta/core/MyController.java
+++ jboss/src/main/java/penta/core/MyController.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyController {
 
 	
-	@RequestMapping("/penta")
+	
+	@RequestMapping("/test")
 	public String penta() {
 		return "Hello, applicaiton is running on JBOSS EAP/crontab real test!!";
 	}
